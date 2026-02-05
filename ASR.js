47579399ab0c03ec50929e1b3bb6dd875f757d92
Mr Boylee <126541418+MrBoylee@users.noreply.github.com>
2026-02-05 07:46:40 +0100

    Update ASR.js

--- ASR.js
+++ ASR.js
@@ -3,7 +3,7 @@
       type: 'gear',
       title: 'Capcut + AI Green Screen Mastery Course COW 1.0',
       price: '$0.0',
-      image: 'https://mrboylee.github.io/Mrboylee-Website-Images/Capcut_Ai_Green_Screen_Mastery.jpg',
+      image: 'https://mrboylee.github.io/Mrboylee-Website-Images/Cow 1.0 (3).jpg',
       button: 'Get for Free',
       class: 'btn-free',
       link: 'https:/boyleedalton.gumroad.com/l/rxpaf'
